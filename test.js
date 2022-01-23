229119df31d160eecad1e1e585443af92b75dcd5
pengjie <37610029@qq.com>
2022-01-23 16:47:52 +0800

    image
    Signed-off-by: pengjie <37610029@qq.com>

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
-for (var i = 0; i < 10; i++) {
-  ((j) => {
-    setTimeout(function () {
-      console.log(j)//1-10
-    }, 1000)
-  })(i)
+let arr = [
+  { id: 1, name: '部门1', pid: 0 },
+  { id: 2, name: '部门2', pid: 1 },
+  { id: 3, name: '部门3', pid: 1 },
+  { id: 4, name: '部门4', pid: 3 },
+  { id: 5, name: '部门5', pid: 4 },
+]
 
+/**
+ * 递归查找，获取children
+ */
+const getChildren = (data, children, pid) => {
+  for (const item of data) {
+    if (item.pid === pid) {
+      const newItem = { ...item, children: [] };
+      children.push(newItem);
+      getChildren(data, newItem.children, item.id);
+    }
+  }
 }
+
+/**
+* 转换方法
+*/
+const arrayToTree = (data, pid) => {
+  const result = [];
+  getChildren(data, result, pid)
+  return result;
+}
+console.log(arrayToTree(arr, 0))
